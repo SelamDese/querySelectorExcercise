3b5f3f678bde9c12deb2cefae6e458c1e35cc8a9
selam <selam0315@gmail.com>
2018-11-27 13:12:23 -0600

    index.html blog.css and main.js files added

---
+++ main.js
@@ -0,0 +1,24 @@
+// Use JavaScript to obtain a reference to the first section with the 
+// class of article_header and change its text with the textContent property to 
+// "Welcome to the {insert your name here} blog"
+
+let headerEl = document.querySelector('.article__header');
+headerEl.textContent = "Welcome to the Selam blog" ;
+
+// Use JavaScript to obtain a reference to all article__header elements
+//  and change their classList property value to article__header important.
+
+let headerE2 = document.querySelectorAll('.article__header');
+for(let i=0 ; i<headerE2.length ; i++){
+headerE2[i].classList.replace('article__header' , 'article__header__important');}
+
+// Obtain a reference the element with a class of dashed and remove it.
+
+let dashedE1 = document.querySelector('.dashed');
+dashedE1.classList.remove('dashed');
+
+// Obtain a reference the element with a class of article_footer and 
+// add the class of goldenrod it.
+let footerE1 = document.querySelector('.article__footer');
+footerE1.classList.add('goldenrod');
+debugger
